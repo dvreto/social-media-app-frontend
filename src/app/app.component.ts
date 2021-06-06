84f8116f7f2b0984e10fdd86ee1bb1cb80000961
dvreto <vretoski@hotmail.com>
2021-06-06 23:39:11 +0200

    Fixed Styling Issue on post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Posts } from './app.model';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PostService } from './post.service';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,22 +13,25 @@ export class AppComponent {
   title = 'social-media-app';
   firstName = 'daniel';
   lastName = 'vretoski';
-  avatar = 'avatar';
+  avatar = '../assets/pics/unnamed.png';
+  ParagraphChecker:boolean = false;
 
   posts: Posts[] = [];
 
-
-
   postMessage(form: NgForm): void {
     console.log(form.value.description);
+    
+    if (form.value.description !="" && form.value.description !=null ) {
+      
+    this.ParagraphChecker = false;
     let post: Posts = new Posts(this.firstName, this.lastName, this.avatar, form.value.description )
     this.posts.push(post);
-
+    form.resetForm();
     
-    
-    
+    }
+    else{
+      this.ParagraphChecker = true;
+    }
   }
-
-
 }
 
